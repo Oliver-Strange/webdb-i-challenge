c60cd2d31d336830f672e4ce1082932a30c8938c
Oliver-Strange <preston.burton@gmail.com>
2019-06-10 13:45:36 -0700

    adds basic CRUD template

--- server.js
+++ server.js
@@ -1,7 +1,46 @@
-const express = require('express');
+const express = require("express");
 
+const accountsDb = require("./data/accounts-model");
 const server = express();
 
-// your code here
+// GET
+server.get("", async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// GET by Id
+server.get("", async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// POST
+server.post("", async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// PUT
+server.put("", async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// DELETE
+server.delete("", async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = server;
